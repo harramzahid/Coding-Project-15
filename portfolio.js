20301ec7a8ca8a790a71ab7b6160901f5626125a
Harram Zahid <harramzahid5@gmail.com>
2024-11-10 22:51:45 -0500

    adding comments

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,13 @@
-// portfolio.js
+// Task 2 Create the portfolio Module
+// this is the portfolio.js file
 import { assets } from './asset.js';
 
-// Function to calculate total portfolio value
+// Creating the function to calculate total portfolio value
 export function calculatePortfolioValue() {
   return assets.reduce((total, asset) => total + (asset.price * asset.quantity), 0);
 }
 
-// Function to calculate portfolio allocation as percentage of total value
+// Creating the function to calculate portfolio allocation as percentage of total value
 export function getPortfolioAllocation() {
   const totalValue = calculatePortfolioValue();
   return assets.map(asset => ({
